80caa32b468320e905e84f6255c44304be60b557
sernst <swernst@gmail.com>
2013-11-25 09:45:27 -0800

    Expanded large message testing.

--- test/test_ping.py
+++ test/test_ping.py
@@ -10,8 +10,9 @@ print 'PING:', result.echo(True, True)
 result = conn.echo('This is a test')
 print 'ECHO:', result.echo(True, True)
 
-largeMessage = StringUtils.getRandomString(64000)
-result = conn.echo(largeMessage)
-print 'LARGE ECHO:', result.echo(True, True)
+for i in range(100):
+    largeMessage = StringUtils.getRandomString(64000)
+    result = conn.echo(largeMessage)
+    print 'LARGE ECHO[#%s]:' % i, result.echo(True, True)
 
 print 'Operation complete'
